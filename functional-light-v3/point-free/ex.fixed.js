fc24b9b86c6d7810828c88c5d1b119e3920a6059
George <admin@georgesimos.com>
2020-03-28 14:31:29 +0200

    point free

--- functional-light-v3/point-free/ex.fixed.js
+++ functional-light-v3/point-free/ex.fixed.js
@@ -1,19 +1,19 @@
 "use strict";
 
 function not(predicate) {
-	return function negated(...args){
-		return !predicate( ...args );
-	};
+  return function negated(...args) {
+    return !predicate(...args);
+  };
 }
 
 function when(fn) {
-	return function(predicate){
-		return function(...args){
-			if (predicate( ...args )) {
-				return fn( ...args );
-			}
-		};
-	};
+  return function(predicate) {
+    return function(...args) {
+      if (predicate(...args)) {
+        return fn(...args);
+      }
+    };
+  };
 }
 
 // *********************
@@ -24,13 +24,13 @@ var printIf = when(output);
 var isLongEnough = not(isShortEnough);
 
 function isShortEnough(str) {
-	return str.length <= 5;
+  return str.length <= 5;
 }
 
 var msg1 = "Hello";
 var msg2 = msg1 + " World";
 
-printIf(isShortEnough)(msg1);		// Hello
+printIf(isShortEnough)(msg1); // Hello
 printIf(isShortEnough)(msg2);
 printIf(isLongEnough)(msg1);
-printIf(isLongEnough)(msg2);		// Hello World
+printIf(isLongEnough)(msg2); // Hello World
--- functional-light-v3/point-free/ex.js
+++ functional-light-v3/point-free/ex.js
@@ -1,29 +1,33 @@
 "use strict";
 
-function output(txt) {
-	console.log(txt);
+function not(predicate) {
+  return function nagated(...args) {
+    return !predicate(...args);
+  };
 }
 
-function printIf(shouldPrintIt) {
-	return function(msg) {
-		if (shouldPrintIt(msg)) {
-			output(msg);
-		}
-	};
+function when(fn) {
+  return function(predicate) {
+    return function(...args) {
+      if (predicate(...args)) {
+        return fn(...args);
+      }
+    };
+  };
 }
+
+var output = console.log.bind(console);
+var printIf = when(output);
+var isLongEnough = not(isShortEnough);
 
 function isShortEnough(str) {
-	return str.length <= 5;
-}
-
-function isLongEnough(str) {
-	return !isShortEnough(str);
+  return str.length <= 5;
 }
 
 var msg1 = "Hello";
 var msg2 = msg1 + " World";
 
-printIf(isShortEnough)(msg1);		// Hello
+printIf(isShortEnough)(msg1); // Hello
 printIf(isShortEnough)(msg2);
 printIf(isLongEnough)(msg1);
-printIf(isLongEnough)(msg2);		// Hello World
+printIf(isLongEnough)(msg2); // Hello World
